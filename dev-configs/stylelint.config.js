f5b52b5d379c3dcb72c0c7ee23f4c9a984328f78
Akash Patel <akashspatel99@gmail.com>
2025-05-05 00:16:06 -0400

    added security checks for linting

--- dev-configs/stylelint.config.js
+++ dev-configs/stylelint.config.js
@@ -6,12 +6,33 @@ module.exports = {
   rules: {
     "block-no-empty": true,
     "color-no-invalid-hex": true,
+    "declaration-no-important": true,
+    "declaration-property-value-disallowed-list": {
+      "display": ["none"],
+      "visibility": ["hidden"],
+      "opacity": ["0"]
+    },
     "font-family-no-duplicate-names": true,
+    "function-url-no-scheme-relative": true,
+    "function-url-scheme-disallowed-list": ["http", "https"],
     "no-empty-source": true,
-    "order/properties-alphabetical-order": true
+    "no-invalid-position-at-import-rule": true,
+    "order/properties-alphabetical-order": true,
+    "plugin/use-defensive-css": [true, {
+      "severity": "error",
+      "accidental-hover": true,
+      "background-repeat": true,
+      "custom-property-fallbacks": true,
+      "flex-wrapping": true,
+      "scroll-chaining": true,
+      "scrollbar-gutter": true,
+      "vendor-prefix-grouping": true,
+    }],
+    "selector-max-specificity": "0,3,0",
   },
   plugins: [
-		"stylelint-order"
+		"stylelint-plugin-defensive-css",
+    "stylelint-order"
 	],
   overrides: [
     {
